be57fc25ad287ea485558e87910ed46f26744c5e
Nguyen, Thanh D.T <n.tthanh@student.fontys.nl>
2018-09-26 15:45:09 +0200

    Integrate MonoGame Framework with Windows Form

---
+++ IndoorFootballStrategySimulator/MonoGameWindow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Forms.Controls;
+
+namespace IndoorFootballStrategySimulator {
+    class MonoGameWindow : UpdateWindow {
+
+        private float frameRate;
+
+        protected override void Initialize() {
+            base.Initialize();
+        }
+
+        protected override void Update(GameTime gameTime) {
+            base.Update(gameTime);
+
+            frameRate = 1 / (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+
+        protected override void Draw() {
+            base.Draw();
+
+            Editor.spriteBatch.Begin();
+            Editor.spriteBatch.DrawString(Editor.Font, $"fps: { frameRate.ToString("0.0") }", new Vector2(10f, 10f), Color.White);
+            Editor.spriteBatch.End();
+        }
+
+    }
+}
--- IndoorFootballStrategySimulator/Program.cs
+++ IndoorFootballStrategySimulator/Program.cs
@@ -1,21 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace IndoorFootballStrategySimulator {
-#if WINDOWS || LINUX
-    /// <summary>
-    /// The main class.
-    /// </summary>
-    public static class Program
-    {
+    static class Program {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
-        {
-            using (var game = new Simulator())
-                game.Run();
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
-#endif
 }
