66726db3470775d07b9c4433cb357df0e51c9c76
Viet Hai Nguyen <v.nguyen@student.fontys.nl>
2018-10-04 11:30:42 +0200

    Revert "nothing changed"
    This reverts commit 8dc810f930cccc106e41ac34521a9eb2210bb9ac.

--- IndoorFootballStrategySimulator/Form1.Designer.cs
+++ IndoorFootballStrategySimulator/Form1.Designer.cs
@@ -31,7 +31,7 @@
             this.monoGameWindow1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.monoGameWindow1.Location = new System.Drawing.Point(0, 0);
             this.monoGameWindow1.Name = "monoGameWindow1";
-            this.monoGameWindow1.Size = new System.Drawing.Size(1370, 749);
+            this.monoGameWindow1.Size = new System.Drawing.Size(1424, 861);
             this.monoGameWindow1.TabIndex = 0;
             this.monoGameWindow1.Text = "monoGameWindow1";
             // 
@@ -39,7 +39,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.ClientSize = new System.Drawing.Size(1424, 861);
             this.Controls.Add(this.monoGameWindow1);
             this.Name = "Form1";
             this.Text = "Form1";
