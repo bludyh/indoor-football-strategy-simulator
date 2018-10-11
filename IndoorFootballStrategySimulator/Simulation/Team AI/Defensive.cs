55c4df2e2a6a67017784c4ff4ad652b64249a52a
Viet Hai Nguyen <v.nguyen@student.fontys.nl>
2018-10-11 19:45:18 +0200

    Create TeamAI class

---
+++ IndoorFootballStrategySimulator/Simulation/Team AI/Defensive.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndoorFootballStrategySimulator.Simulation
+{
+    public class Defensive: State<Team>
+    {
+        private static Defensive instance = new Defensive();
+        public static Defensive Instance()
+        {
+            return instance;
+        }
+        public override void OnEnter(Team team)
+        {
+
+        }
+        public override void Handle(Team team)
+        {
+
+        }
+        public override void OnExit(Team team)
+        {
+        }
+    }
+}
---
+++ IndoorFootballStrategySimulator/Simulation/Team AI/Offensive.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndoorFootballStrategySimulator.Simulation
+{
+    public class Offensive:State<Team>
+    {
+        private static Offensive instance = new Offensive();
+        public static Offensive Instance()
+        {
+            return instance;
+        }
+        public override void OnEnter(Team team)
+        {
+
+        }
+        public override void Handle(Team team)
+        {
+
+        }
+        public override void OnExit(Team team)
+        {
+        }
+    }
+}
